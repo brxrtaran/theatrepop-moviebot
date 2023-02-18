c81dbec21bf7643feae81e72320f6529425eaa26
brxrtaran <116512395+brxrtaran@users.noreply.github.com>
2023-02-18 21:38:34 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 url_list = {}
-api_key = "ENTER YOUR API KEY HERE"
+api_key = "d46d4b4d0bfa8ddba96ec07cb1ccc07a8d10425b"
 
 
 def search_movies(query):
@@ -32,7 +32,7 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://api.shareus.in/shortLink?token={$shareus_token}&link={i['href']}"
+            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
             response = requests.get(url)
             link = response.json()
             final_links[f"{i.text}"] = link['shortenedUrl']
